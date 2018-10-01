7c1be0600331c6485a05886f0cce4f67b42bda5e
Boban Acimovic <boban.acimovic@gmail.com>
2018-10-01 18:48:26 +0200

    try with channels

--- app.go
+++ app.go
@@ -29,34 +29,61 @@ func main() {
 
 	c := cron.New()
 	c.AddFunc("0,30 * * * * *", func() {
-		log.Printf("%s cron trigger", me)
+		log.Printf("%s cron start", me)
+		defer log.Printf("%s cron end", me)
 		opts := &api.LockOptions{
 			Key:        fmt.Sprintf("service/%s/lock/", sn),
 			Value:      []byte(fmt.Sprintf("set by %s", me)),
-			SessionTTL: "10s",
+			SessionTTL: "60s",
 		}
 
 		lock, err := consul.LockOpts(opts)
 		if err != nil {
-			log.Printf("%s Lock: %v", me, err)
+			log.Printf("%s LockOpts: %v", me, err)
 			return
 		}
 
-		_, err = lock.Lock(nil)
-		if err != nil {
-			log.Printf("%s Lock: %v", me, err)
-			return
-		}
-		log.Printf("%s lock, doing something", me)
-		time.Sleep(20 * time.Second)
-		log.Printf("%s unlock", me)
-		err = lock.Unlock()
-		if err != nil {
-			log.Printf("%s unlock: %v", me, err)
-		}
+		haveLock := false
 
-		// <-lockCh
-		// log.Println(me, 2)
+		stop := make(chan struct{})
+		time.AfterFunc(5*time.Second, func() {
+			if !haveLock {
+				close(stop)
+			}
+		})
+
+		lead := make(chan struct{})
+		go func() {
+			log.Printf("%s before lock", me)
+			_, err = lock.Lock(stop)
+			log.Printf("%s after lock", me)
+			if err != nil {
+				log.Printf("%s Lock: %v", me, err)
+				return
+			}
+			select {
+			case <-stop:
+				return
+			default:
+				log.Printf("%s lock", me)
+				haveLock = true
+				close(lead)
+			}
+
+		}()
+
+		select {
+		case <-lead:
+			log.Printf("%s working", me)
+			time.Sleep(15 * time.Second)
+			log.Printf("%s unlock", me)
+			err = lock.Unlock()
+			if err != nil {
+				log.Printf("%s unlock: %v", me, err)
+			}
+		case <-stop:
+			log.Printf("%s aborting", me)
+		}
 	})
 	c.Start()
 	defer c.Stop()
